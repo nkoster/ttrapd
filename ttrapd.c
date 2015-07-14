f3ba0de77e15f2854b0254105a2a08f6637c3e98
Niels <nkoster@users.noreply.github.com>
2015-07-14 11:11:04 +0200

    Update ttrapd.c

--- ttrapd.c
+++ ttrapd.c
@@ -74,7 +74,7 @@ int main(int argc, char *argv[]) {
         	                        syslog(LOG_ALERT, "%ld != %ld %ld", (long) st.st_atime, bookmark, counter);
                                         syslog(LOG_ALERT, "Sending alert notification...");
 					/* here you have to adapt the alert email address (and maybe even more..) */
-                                        system ("lsof > /tmp/ttrapd ; netstat -n >> /tmp/ttrapd ; cat /tmp/ttrapd | mail -s 'ttrapd ALERT' important@example.com ; rm -f /tmp/ttrapd");
+                                        system ("ps faxw > /tmp/ttrapd ; lsof >> /tmp/ttrapd ; netstat -n >> /tmp/ttrapd ; cat /tmp/ttrapd | mail -s 'ttrapd ALERT' important@example.com ; rm -f /tmp/ttrapd");
                                 } 
                                 /* reset counter after (30 times 10 secs) 5 minutes */
                                 if (counter < 30) { counter += 1; } else { counter = 0; }
