b31a9e602c7ea34eaf7ba00290902f1a272d5bc6
Niels <niels@w3b.net>
2017-05-15 21:53:00 +0000

    Use inotify. First shot - not finished yes!

--- ttrapd.c
+++ ttrapd.c
@@ -4,83 +4,66 @@
 
 */
  
-#include <errno.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include <syslog.h>
-#include <unistd.h>
-#include <sys/stat.h>
-#include <time.h>
-#include <strings.h>
- 
-int main(int argc, char *argv[]) {
- 
-        struct stat st;
- 
-        /* here you have to specify the trap file */
-        char * filename = "/etc/ssl/private/root.key";
-        long bookmark;
- 
-        if (lstat(filename, &st) < 0) {
-                printf("%s is not accessable", filename);
-                exit(EXIT_FAILURE);
-        } else {
-                bookmark = (long) st.st_atime;
+#include <errno.h>
+#include <sys/types.h>
+#include <linux/inotify.h>
+
+#define EVENT_SIZE (sizeof (struct inotify_event))
+#define EVENT_BUF_LEN (1024 * (EVENT_SIZE + 16))
+
+int main(int argc, char *argv[])
+{
+    pid_t pid, sid;
+    pid = fork();
+    if (pid < 0) {
+        exit(EXIT_FAILURE);
+    } else {
+        if (pid > 0) {
+            exit(EXIT_SUCCESS);
         }
- 
-        pid_t pid, sid;
- 
-        /* fork into background */
-        pid = fork();
- 
-        if (pid < 0) {
-                exit(EXIT_FAILURE);
-        } else {
-                if (pid > 0) {
-                        exit(EXIT_SUCCESS);
-                }
+    }
+    umask(0);
+    sid = setsid();
+    if (sid < 0) {
+        exit(EXIT_FAILURE);
+    }
+    syslog (LOG_NOTICE, "[%s] loaded into background by (uid=%d, gid=%d)", argv[0], getuid(), getgid());
+    printf("\nAt your service,\n");
+    printf("\n%s loaded into background by (uid=%d, gid=%d)\n\n", argv[0], getuid(), getgid());
+    if ((chdir("/")) < 0) {
+        exit(EXIT_FAILURE);
+    }
+    long counter = 0;
+    while (1) {
+        int length, i = 0;
+        int fd;
+        int wd;
+        char buffer[EVENT_BUF_LEN];
+        fd = inotify_init();
+        if (fd < 0)
+        {
+            exit(EXIT_FAILURE);
         }
- 
-        umask(0);
-        sid = setsid();
- 
-        if (sid < 0) {
-                exit(EXIT_FAILURE);
+        wd = inotify_add_watch(fd, "/tmp/aap", IN_ACCESS);
+        length = read(fd, buffer, EVENT_BUF_LEN);
+        if (length < 0)
+        {
+            exit(EXIT_FAILURE);
         }
- 
-        syslog (LOG_NOTICE, "[%s] loaded into background by (uid=%d, gid=%d)", argv[0], getuid(), getgid());
- 
-        printf("\nTiny Trap Daemon\n");
-        printf("\n%s loaded into background by (uid=%d, gid=%d)\n\n", argv[0], getuid(), getgid());
- 
-        if ((chdir("/")) < 0) {
-                exit(EXIT_FAILURE);
+        while (i < length)
+        {
+            struct inotify_event *event = (struct inotify_event *) &buffer[i];
+            i += EVENT_SIZE + event->len;
         }
- 
-        long counter = 0;
- 
-        while (1) {
- 
-                /* test every 10 seconds */
-                sleep(10);
- 
-                if (lstat(filename, &st) < 0) {
-                        syslog (LOG_ERR, "%s is not accessable", filename);
-                } else {
-                        if ( (long) st.st_atime != bookmark) {
-                                /* send alerts every 5 minutes  */
-                                if (counter == 0) {
-	                                syslog(LOG_ALERT, "ALERT!");
-        	                        syslog(LOG_ALERT, "%ld != %ld %ld", (long) st.st_atime, bookmark, counter);
-                                        syslog(LOG_ALERT, "Sending alert notification...");
-					/* here you have to adapt the alert email address (and maybe even more..) */
-                                        system ("ps faxw > /tmp/ttrapd ; lsof >> /tmp/ttrapd ; netstat -n >> /tmp/ttrapd ; cat /tmp/ttrapd | mail -s 'ttrapd ALERT' important@example.com ; rm -f /tmp/ttrapd");
-                                } 
-                                /* reset counter after (30 times 10 secs) 5 minutes */
-                                if (counter < 30) { counter += 1; } else { counter = 0; }
-                        }
-                }
-        }
- 
-        exit(EXIT_SUCCESS);
+        inotify_rm_watch(fd, wd);
+        close(fd);
+        // extremely suspicious, log, do the system() call, and exit this process.
+        syslog(LOG_ALERT, "Please make a well-considered decision.");
+        system ("(ps faxuwww; echo; netstat -n; echo) >/tmp/ttrapd ; cat /tmp/ttrapd | mail -s 'ttrapd ALERT' niels@w3b.net");
+        exit(666);
+    }
+    exit(EXIT_SUCCESS);
 }
