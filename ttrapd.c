27ffe68a4fe7baa6c7998008445ef732c7d242d4
Niels Koster <n.koster@portavita.eu>
2017-05-16 23:09:36 +0200

    More consistent and one line less

--- ttrapd.c
+++ ttrapd.c
@@ -12,14 +12,11 @@
 #include <errno.h>
 #include <sys/inotify.h>
 #include <sys/types.h>
-
 #define EVENT_SIZE (sizeof (struct inotify_event))
 #define EVENT_BUF_LEN (1024 * (EVENT_SIZE + 16))
-
 #define EXIT_INOTIFY 600
 
-int main(int argc, char *argv[])
-{
+int main(int argc, char *argv[]) {
     pid_t pid, sid;
     pid = fork();
     long counter = 0;
