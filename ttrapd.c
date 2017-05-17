c7dafc080d805f1af4e5a9738e4329f21c8f10d1
Niels Koster <n.koster@portavita.eu>
2017-05-17 10:35:25 +0200

    Add missing includes

--- ttrapd.c
+++ ttrapd.c
@@ -9,9 +9,11 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <syslog.h>
+#include <unistd.h>
 #include <errno.h>
 #include <sys/inotify.h>
 #include <sys/types.h>
+#include <sys/stat.h>
 #define EVENT_SIZE (sizeof (struct inotify_event))
 #define EVENT_BUF_LEN (1024 * (EVENT_SIZE + 16))
 #define EXIT_INOTIFY 600
